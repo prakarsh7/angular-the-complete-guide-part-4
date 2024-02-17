30718b49cd1259e7dc9dbc7c0a95fd6d7047c05f
Prakarsh Gour <prakarshgour0900@gmail.com>
2024-02-17 13:41:20 +0530

    Add solution for assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsernameComponent } from './username/username.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/username/username.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrl: './username.component.css'
+})
+export class UsernameComponent {
+  username = '';
+  isUsername = false;
+}
